3e10d9bfb7b10a8129138a67049fffbbea122c0b
Bastian8004 <paszko08042004@gmail.com>
2024-10-12 17:34:40 +0200

    settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Załaduj zmienne środowiskowe
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# B.uild paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
