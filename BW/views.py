2f0faf850e6a3977493d0cfe08d84b895dd85315
Bastian8004 <paszko08042004@gmail.com>
2024-10-31 20:29:16 +0100

    bramka update

--- BW/views.py
+++ BW/views.py
@@ -409,40 +409,41 @@ def cancel(request):
 
 @csrf_exempt
 def stripe_webhook(request):
-    if request.method != "POST":
-        return HttpResponse(status=405)  # Method Not Allowed
+    if request.method == "GET":
+        return HttpResponse("This endpoint only accepts POST requests.", status=405)
 
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-    endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
-    payload = request.body
-    sig_header = request.META.get("HTTP_STRIPE_SIGNATURE")
-    event = None
+    if request.method == "POST":
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+        payload = request.body
+        sig_header = request.META.get("HTTP_STRIPE_SIGNATURE")
+        event = None
 
-    try:
-        event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
-    except ValueError:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError:
-        # Invalid signature
-        return HttpResponse(status=400)
+        try:
+            event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
+        except ValueError:
+            # Invalid payload
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError:
+            # Invalid signature
+            return HttpResponse(status=400)
 
-    # handle the checkout.session.completed event
-    if event["type"] == "checkout.session.completed":
-        session = event["data"]["object"]
+        # handle the checkout.session.completed event
+        if event["type"] == "checkout.session.completed":
+            session = event["data"]["object"]
 
-        # Fetch all the required data from session
-        client_reference_id = session.get('client_reference_id')
-        stripe_customer_id = session.get('customer')
-        stripe_subscription_id = session.get("subscription")
+            # Fetch all the required data from session
+            client_reference_id = session.get('client_reference_id')
+            stripe_customer_id = session.get('customer')
+            stripe_subscription_id = session.get("subscription")
 
-        # Get the user and create a new StripeCustomer
-        user = User.objects.get(id=client_reference_id)
-        StripeCustomer.objects.create(
-            user=user,
-            stripeCustomerId=stripe_customer_id,
-            stripeSubscriptionId=stripe_subscription_id,
-        )
-        print(f"{user.username} subskrybuje.")
+            # Get the user and create a new StripeCustomer
+            user = User.objects.get(id=client_reference_id)
+            StripeCustomer.objects.create(
+                user=user,
+                stripeCustomerId=stripe_customer_id,
+                stripeSubscriptionId=stripe_subscription_id,
+            )
+            print(f"{user.username} subskrybuje.")
 
-    return HttpResponse(status=200)
+        return HttpResponse(status=200)
