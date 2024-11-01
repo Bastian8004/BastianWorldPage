548c016ca7a2e298f238b566536c14b13b5ef1b0
Bastian8004 <paszko08042004@gmail.com>
2024-11-01 17:37:39 +0100

    bramka update

--- BW/templatetags/timetags.py
+++ BW/templatetags/timetags.py
@@ -1,12 +1,29 @@
-# timestamp conversion
 import datetime
 from django import template
+import locale
 
 register = template.Library()
-def print_timestamp(timestamp):
+
+# Ustawienie polskiego języka dla formatowania
+try:
+    locale.setlocale(locale.LC_TIME, 'pl_PL.UTF-8')  # lub 'pl_PL' zależnie od systemu
+except locale.Error:
+    print("Nie udało się ustawić lokalizacji na polski. Używany jest domyślny format.")
+
+def print_timestamp(timestamp, format_type="YMD"):
+    """
+    Funkcja przetwarza znacznik czasu na format daty.
+    Parametr format_type pozwala wybrać format: "YMD" dla YYYY-MM-DD lub "PL" dla polskiego formatu.
+    """
     try:
         ts = float(timestamp)
-    except ValueError:
+        date_obj = datetime.datetime.fromtimestamp(ts)
+    except (ValueError, TypeError):
         return None
-    return datetime.datetime.fromtimestamp(ts)
-register.filter(print_timestamp)
+
+    if format_type == "PL":
+        return date_obj.strftime('%d.%m.%Y')  # Format polski DD.MM.YYYY
+    else:
+        return date_obj.strftime('%Y-%m-%d')  # Format YYYY-MM-DD
+
+register.filter("print_timestamp", print_timestamp)
